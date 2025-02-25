d5dff83f56e60d1fa7db528b1a9017f8e705cfe2
diego chinchilla <diegofch29@hotmail.com>
2025-02-25 16:48:14 -0500

    solved javascript challenge

---
+++ Test.js
@@ -0,0 +1,43 @@
+/* TEST JS */
+/*
+    1. Problem 1 - (A)
+    Create an Object with a "hello" method that writes "Hello <name> in the console"
+*/
+
+/*
+    2. Problem 1 - (B)
+    How would you make name inmutable?
+    (Can write or just describe)
+*/
+
+/*
+    3. Problem 2
+    Write a funtion that logs the 5 cities that occur the most in the array below in
+    order from the most number of occurrences to least.
+*/
+
+const citiesList = [
+    "nasville",
+    "nasville",
+    "los angeles",
+    "nasville",
+    "Madrid",
+    "memphis",
+    "barcelona",
+    "los angeles",
+    "sevilla",
+    "Madrid",
+    "canary islands",
+    "barcelona",
+    "Madrid",
+    "Madrid",
+    "nasville",
+    "barcelona",
+    "london",
+    "berlin",
+    "Madrid",
+    "nasville",
+    "london",
+    "Madrid",
+    "Madrid",
+]
